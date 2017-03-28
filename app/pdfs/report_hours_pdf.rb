7e1d2cbb759bd5469c5fb19dd1d0a9d428470df7
Muhammad Ammar Nadeem <ammar.nadeem@pikessoft.com>
2017-03-28 19:46:11 +0500

    error resolving in pdf generation

--- app/pdfs/report_hours_pdf.rb
+++ app/pdfs/report_hours_pdf.rb
@@ -5,29 +5,31 @@ class ReportHoursPdf < Prawn::Document
 		@users = users
 		text "Date: #{Date.today.strftime("%m/%d/%Y")}"
 		@users.each do |user|
-			move_down 20
-			table [["Member Name", user.fullname ]], width: bounds.width, column_widths: {0 => 95}
-			move_down 20
-			table [["Strategic Plan", user.objectives.first.plans.first.name ]], width: bounds.width, column_widths: {0 => 95}
-			move_down 20
-			table [["Total Hours Assigned", user.objectives.pluck(:actual_hours).sum]], width: bounds.width, column_widths: {0 => 130}	
-			move_down 20
-			text "Total Hours by Objective:"			
-			dynamic_data = []
-			arr_head = []
-			arr_head << "Hours"
-			arr_head << "Objective"
-			dynamic_data << arr_head
-			user.objectives.each do |obj|
-				arr = []
-				arr << obj.actual_hours.to_s
-				arr << obj.description 
-				dynamic_data << arr
-			end
-			move_down 10
-			table dynamic_data, width: bounds.width, column_widths: {0 => 50}
-			if user != @users.last
-				start_new_page
+			if user.objectives.present?
+				move_down 20
+				table [["Member Name", user.fullname ]], width: bounds.width, column_widths: {0 => 95}
+				move_down 20
+				table [["Strategic Plan", user.objectives.first.plans.first.name ]], width: bounds.width, column_widths: {0 => 95}
+				move_down 20
+				table [["Total Hours Assigned", user.objectives.pluck(:actual_hours).sum]], width: bounds.width, column_widths: {0 => 130}	
+				move_down 20
+				text "Total Hours by Objective:"			
+				dynamic_data = []
+				arr_head = []
+				arr_head << "Hours"
+				arr_head << "Objective"
+				dynamic_data << arr_head
+				user.objectives.each do |obj|
+					arr = []
+					arr << obj.actual_hours.to_s
+					arr << obj.description 
+					dynamic_data << arr
+				end
+				move_down 10
+				table dynamic_data, width: bounds.width, column_widths: {0 => 50}
+				if user != @users.last
+					start_new_page
+				end
 			end
 		end
 	end
