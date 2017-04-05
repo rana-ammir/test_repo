cceaf9e762910d2c2ba7822b67466dd24a9e1f2a
Muhammad Ammar Nadeem <ammar.nadeem@pikessoft.com>
2017-04-05 18:11:26 +0500

    convert migration into pg format

--- db/migrate/20170405081159_change_organization_id_to_users.rb
+++ db/migrate/20170405081159_change_organization_id_to_users.rb
@@ -1,5 +1,5 @@
 class ChangeOrganizationIdToUsers < ActiveRecord::Migration
   def change
-  	change_column :users, :organization_id, :integer
+  	change_column :users, :organization_id, 'integer USING CAST(organization_id AS integer)'
   end
 end
